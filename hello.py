78bd1b7afaeed8f9afdfd1b5170aec3014e3258a
Eduarda <eduardacristovaops.gmail.com>
2025-08-28 01:16:41 +0000

    Atividade 03

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return '<p>Alterações por meio do GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<username>')
+def user_profile(username):
+    return render_template('user.html', name=username)
+
+@app.route('/rotainexistente')
+def not_found_route():
+    return render_template('404.html'), 404
+
+
